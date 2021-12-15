c098a5e55a1d8cb8a07b6931ea0173e37b715ddb
Glazkov Pavel <glazprom@mail.ru>
2021-12-15 08:49:23 +0300

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ListComponent } from './field/list/list.component';
+import { CardComponent } from './field/card/card.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FieldComponent } from './field/field.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    ListComponent,
+    CardComponent,
+    NotFoundComponent,
+    HeaderComponent,
+    FooterComponent,
+    FieldComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
